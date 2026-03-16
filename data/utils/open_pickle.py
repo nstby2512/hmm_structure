ab34925b38891e64a4792a3fc33c41c10db75952
nstby2512 <143398921+nstby2512@users.noreply.github.com>
2026-03-16 22:45:27 +0800

    gitignore

--- data/utils/open_pickle.py
+++ data/utils/open_pickle.py
@@ -4,7 +4,7 @@ from itertools import islice
 with open('data/childes_by_stage/mature/mature.derivations.p', 'rb') as f:
     data = pickle.load(f)
 
-der = data['91-0699']
+der = data['91-0700']
 detail_rule = der[2]
 pprint(der)
 #pprint(data, width=80)
